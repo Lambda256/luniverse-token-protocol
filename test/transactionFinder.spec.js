53ccfb99d13a191d66c19ed8f3c23b7b7345ed61
willy-kimbyungsoo <willy@lambda256.io>
2021-09-13 15:40:40 +0900

    test: getEvent functions

--- test/transactionFinder.spec.js
+++ test/transactionFinder.spec.js
@@ -1,7 +1,17 @@
-const subject = require('../transactionFinder');
+const subject = require('../eventFinder/transactionFinder');
 
 describe('transactionFinder', () => {
-  describe('getEventList', () => {
-    subject.getEventList();
+  describe('getErc20EventList', () => {
+    it('If the event remains, it will be printed.', async () => {
+      const events = await subject.getErc20EventList({
+        rpcHttpEndpoint: 'http://127.0.0.1:8545',
+        contractAddress: '0x37dbfe6fbcff42aabb653469c5e889cc7faf7f9c',
+        eventName: 'Transfer',
+        fromBlock: 0,
+        toBlock: 5,
+      });
+
+      console.log(JSON.stringify(events));
+    });
   });
 });
